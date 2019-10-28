2d05d50aefcc635b725fa0b1feeb9bf3cc830513
Mohsen994 <mohsen@almakrami.com>
2019-10-28 19:00:08 +0300

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Mohsen Almakrami on 29/02/1441 AH.
+//  Copyright © 1441 Mohsen Almakrami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
